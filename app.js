572384e2752614c6c622a252ae86cc7914bca27f
Camila Roque <59898248+milaroque@users.noreply.github.com>
2020-03-03 17:57:04 -0300

    ultimo commit

--- app.js
+++ app.js
@@ -2,44 +2,44 @@ let name = prompt("Qual o seu nome?");
 alert("Bem Vind@ " + name);
 
 let enterTheGame = prompt("Deseja jogar o Quiz da Laboratória? (Digite o número correspondente a sua resposta) \n 1.Sim \n 2.Não");
-let firstQuestion = prompt("Qual a primeira mulher programadora do mundo? (Digite o número correspondente a sua resposta) \n 1.Maria Joaquina \n 2.Elba Ramalho \n 3.Ada Lovelace");
-let secondQuestion = prompt("QUal o nome da fundadora da Laboratória? (Digite o número correspondente a sua resposta) \n 1.Mariana Costa Checa \n 2.Silvio Santos \n 3.Pepita ");
-let thirdQuestion = prompt("Qual o nome do movimento que permitiu as mulheres conquistarem o direito ao voto? (Digite o número correspondente a sua resposta) \n 1.Movimento Abolicionista \n 2.Movimento Carnavalesco \n 3.Movimento Sufragista");
+
 let firstQuestionAnswer = "";
 let secondQuestionAnswer = "";
 let thirdQuestionAnswer = "";
 let nameWriting = document.getElementById("saudacao");
 nameWriting.innerHTML = "Olá, " + name + "!";
 
-if (enterTheGame === "2") {
+if (enterTheGame === "1") {
+    let firstQuestion = prompt("Qual a primeira mulher programadora do mundo? (Digite o número correspondente a sua resposta) \n 1.Maria Joaquina \n 2.Elba Ramalho \n 3.Ada Lovelace");
+let secondQuestion = prompt("QUal o nome da fundadora da Laboratória? (Digite o número correspondente a sua resposta) \n 1.Mariana Costa Checa \n 2.Silvio Santos \n 3.Pepita ");
+let thirdQuestion = prompt("Qual o nome do movimento que permitiu as mulheres conquistarem o direito ao voto? (Digite o número correspondente a sua resposta) \n 1.Movimento Abolicionista \n 2.Movimento Carnavalesco \n 3.Movimento Sufragista");
+    
+    if (firstQuestion === "3") {
+        firstQuestionAnswer = "Resposta Correta 1"
+    }
+    else {
+        firstQuestionAnswer = "Resposta Incorreta 1"
+    };
+    
+    if (secondQuestion === "1") {
+        secondQuestionAnswer = "Resposta Correta 2"
+    }
+    else {
+        secondQuestionAnswer = "Resposta Incorreta 2"
+    };
+    
+    if (thirdQuestion === "3") {
+        thirdQuestionAnswer = "Resposta Correta 3"
+    }
+    else {
+        thirdQuestionAnswer = "Resposta Incorreta 3"
+    };
+} else if (enterTheGame === "2"){
     alert("Que pena, perdeu um jogo massa! =(");
-}
-else if (enterTheGame === "1") {
-    firstQuestion;
-    secondQuestion;
-    thirdQuestion;
 };
 
-if (firstQuestion === "3") {
-    firstQuestionAnswer = "Resposta Correta 1"
-}
-else {
-    firstQuestionAnswer = "Resposta Incorreta 1"
-};
 
-if (secondQuestion === "1") {
-    secondQuestionAnswer = "Resposta Correta 2"
-}
-else {
-    secondQuestionAnswer = "Resposta Incorreta 2"
-};
 
-if (thirdQuestion === "3") {
-    thirdQuestionAnswer = "Resposta Correta 3"
-}
-else {
-    thirdQuestionAnswer = "Resposta Incorreta 3"
-};
 
 let idOne = document.getElementById("one");
 let idTwo = document.getElementById("two");
